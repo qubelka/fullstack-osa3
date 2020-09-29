7042b6dddff7708ec40ed1a7e8210d7f8d496c52
Qubelka <42945433+qubelka@users.noreply.github.com>
2020-09-29 13:23:23 +0300

    Deploy to Heroku

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.post('/api/persons', (req, res) => {
     res.status(201).json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
