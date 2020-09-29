1c248e199d23bd7d03a5df40524176939d91faea
Qubelka <42945433+qubelka@users.noreply.github.com>
2020-09-29 13:58:10 +0300

    3.11

---
+++ build/precache-manifest.a633eac83d443b0c9b9c9efb28449464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a930bd041472d441350b9d1fc3efaf7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0b154c16ef231710d8e",
+    "url": "/static/css/main.70051b7b.chunk.css"
+  },
+  {
+    "revision": "1a15deb88bce7379561a",
+    "url": "/static/js/2.dbfacc78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dbfacc78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0b154c16ef231710d8e",
+    "url": "/static/js/main.97acae63.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
